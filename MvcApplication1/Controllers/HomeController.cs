f43f185448936e06b98ca1bcb679131f183df885
abdulbasheer <abdulbsr24@gmail.com>
2017-12-04 18:29:10 +0530

    project creat

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;   
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string pname, int uprice, int qty)
+        {
+            int t = uprice * qty;
+            ViewData["prdname"] = pname;
+            ViewBag.total = t;
+            return View();
+        }
+        public ActionResult Details( int dno )
+        {
+            Response.Write("Request Dept"+ dno);
+            return View();
+        }
+        public ActionResult Details2(int id)
+        {
+            Response.Write("Request Dept" + id);
+            return View();
+        }
+    }
+}
---
+++ MvcApplication1/Models/Dept.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class Dept
+    {
+        public int deptno { get; set; }
+        public string Dname { get; set; }
+        public string loc { get; set; }
+    }
+}
